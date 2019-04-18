3b9384a92cc2955b9c6f609c731f0644f413bc25
tomas.a.cerezo@outlook.com <tomas.a.cerezo@outlook.com>
2019-04-18 17:53:41 -0300

    Ej 5 - Inciso 8

--- Guia 0 - Ej5-8)/Program.cs
+++ Guia 0 - Ej5-8)/Program.cs
@@ -10,6 +10,38 @@ namespace Guia_0___Ej5_8_
     {
         static void Main(string[] args)
         {
+            string ingreso;
+            bool ok = false;
+            int numero = 0;
+
+            while(!ok)
+            {
+                Console.Write("Ingrese un numero mayor a 0:");
+                ingreso = Console.ReadLine();
+                ok = int.TryParse(ingreso , out  numero);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numero; i++)
+            {
+                int a = 0;
+                for (int j = 1; j < (i + 1); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        a++;
+                    }
+                }
+                if (a <= 2)
+                {
+                    Console.WriteLine(i);
+                }
+              
+
+                
+            }
+            Console.ReadKey();
         }
     }
 }
